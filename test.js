55ae2fa5219e58236bcc1946c6b68c1968e8bec2
BANGWON LEE <qkddnjs31@gmail.com>
2025-04-04 17:12:21 +0900

    edit : 파일명 변경

--- test.js
+++
@@ -1,9 +0,0 @@
-import { registerRoute } from './main.js'
-import { another } from './pages/another.js'
-import { home } from './pages/home.js'
-import { sub } from './pages/sub.js'
-
-//1번
-registerRoute('/', home())
-registerRoute('/sub', sub())
-registerRoute('/another', another())
